45686cabf168b3c7bc59d13078efe2f5c528eced
Gabriel Ziebol <gabezmm@gmail.com>
2014-11-12 08:19:34 -0600

    init func branch
    this will be merged into main when complete

--- reprint/reprint.ino
+++ reprint/reprint.ino
@@ -1,61 +1,36 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
+int temp;
+int tempLCD;
+int heat = 20;//start at a mid range to start the heating
+const int lowTemp = 1;//min of temp range to extrude
+const int highTemp = 3;//max of temp range to extrude
+int count = 0;
+Servo heater;
 
-
-#define BACKLIGHT_PIN     13
-
-LiquidCrystal_I2C lcd(0x38);  // Set the LCD I2C address
-
-//LiquidCrystal_I2C lcd(0x38, BACKLIGHT_PIN, POSITIVE);  // Set the LCD I2C address
-
-
-// Creat a set of new characters
-const uint8_t charBitmap[][8] = {
-   { 0xc, 0x12, 0x12, 0xc, 0, 0, 0, 0 },
-   { 0x6, 0x9, 0x9, 0x6, 0, 0, 0, 0 },
-   { 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0, 0x0 },
-   { 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0, 0x0 },
-   { 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0x0 },
-   { 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0x0 },
-   { 0x0, 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0x0 },
-   { 0x0, 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0x0 }
-   
-};
-
-void setup()
-{
-   int charBitmapSize = (sizeof(charBitmap ) / sizeof (charBitmap[0]));
-
-  // Switch on the backlight
-  pinMode ( BACKLIGHT_PIN, OUTPUT );
-  digitalWrite ( BACKLIGHT_PIN, HIGH );
-  
-  lcd.begin(16,2);               // initialize the lcd 
-
-   for ( int i = 0; i < charBitmapSize; i++ )
-   {
-      lcd.createChar ( i, (uint8_t *)charBitmap[i] );
-   }
-
-  lcd.home ();                   // go home
-  lcd.print("Hello, ARDUINO ");  
-  lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.print (" FORUM - fm   ");
-  delay ( 1000 );
+void setup(){
+  heater.attach(6);
+  Serial.begin(9600);
 }
 
-void loop()
-{
-   lcd.home ();
-   // Do a little animation by writing to the same location
-   for ( int i = 0; i < 2; i++ )
-   {
-      for ( int j = 0; j < 16; j++ )
+  void loop(){
+  temp = analogRead(1);
+    if (count < 10)
+    {
+      count ++;
+      tempLCD = 0;//equation to convert thermo couple value to deg. this will be used to display on the LCD
+    }
+    else if (count > 10)
+    {
+      if (temp < lowTemp)
       {
-         lcd.print (char(random(7)));
+        heat ++;
       }
-      lcd.setCursor ( 0, 1 );
-   }
-   delay (200);
-}
+      else if (temp > highTemp)
+      {
+        heat --;
+      }
+      count = 0;
+      heater.write(heat);
+    }
+  }
