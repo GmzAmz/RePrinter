06c77e32489d86be8a009f831b410be92ee612d5
maxar6 <maxar6@gmail.com>
2015-01-08 08:55:16 -0600

    Analog Write Untested
    Implemented a way of sending a analog signal to the arduino mega so it
    can do the PWM drive. The uno is to busy to be able to.

--- reprint/reprint.ino
+++ reprint/reprint.ino
@@ -10,15 +10,17 @@ Servo auger;
 Servo heater;                
  
 int spooler_speed(0);   
-int auger_speed(10);  
+int auger_speed(0);  
 int heat_level(0);
-
-int tempSensor = 0;
 
 //These are all the offsets
 int spooler_offset(0);
 int auger_offset(0);
 int heater_offset(0);
+
+int heatPin = 11;
+
+int tempSensor = 0;
 
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
@@ -69,10 +71,12 @@ void setup()
   currTemp = temp.read_temp();
   myPID.SetMode(AUTOMATIC);  //turn the PID on
   
-  spooler.attach(9); //attaches spooler motor controller to pin 11
-  auger.attach(21);  //attaches auger motor controller to pin 6
-  heater.attach(11);  //attaches heater motor controller to pin 5
- 
+  //spooler.attach(9); //attaches spooler motor controller to pin 11
+  //auger.attach(21);  //attaches auger motor controller to pin 6
+  //heater.attach(11);  //attaches heater motor controller to pin 5
+  
+  pinMode(heatPin, OUTPUT);
+   
   Serial.begin(9600);
   lcd.begin(20,4);
   setupLCDChars();
@@ -81,12 +85,14 @@ void setup()
 }
 
 void loop()
-{ 
-  
+{    
   currTemp = temp.read_temp();
   myPID.Compute(); //pid compute
   
   heat_level = (Output/2.834); //conversion from 255 to 90 from PID
+  
+  analogWrite(heatPin, heat_level);
+  
   spooler_offset = (spooler_speed+90); //Conversion from servo to motor controller
   auger_offset = (auger_speed+90);  //Conversion for servo to motor controller
   heater_offset = (heat_level+90);  //Conversion for servo to motor controller
@@ -94,6 +100,8 @@ void loop()
   spooler.write(spooler_offset);  //Writes the spooler speed
   auger.write(auger_offset);      //Writes the auger speed
   heater.write(heater_offset);    //Writes the heater speed
+  
+  Serial.println(heater_offset);
   
   switch (screen){
     case 0:
