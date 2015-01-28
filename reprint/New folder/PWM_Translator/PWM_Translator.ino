a04e198b61afc9cd99583f3d218ea848a5dd6ee2
maxar6 <maxar6@gmail.com>
2015-01-28 09:13:11 -0600

    Cleaned Up the Code

--- reprint/New folder/PWM_Translator/PWM_Translator.ino
+++ reprint/New folder/PWM_Translator/PWM_Translator.ino
@@ -1,16 +1,10 @@
-#include <Servo.h> 
-#include <PID_v1.h>
-
-double Setpoint, Input, Output; //Define Variables we'll be connecting to
-PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT); //Specify the links and initial tuning parameters
-
-int incomingbyte = 0;
+#include <Servo.h>
 
 Servo spooler;  // a maximum of eight servo objects can be created 
 Servo auger;
 Servo heater;                
  
-int spooler_speed(90); 
+int spooler_speed(0); 
 int auger_speed(0);  
 int heat_level(0);
 
@@ -20,26 +14,20 @@ int heater_offset(0);
 
 void setup() 
 {   
-  spooler.attach(10); //attaches spooler motor controller to pin 11
+  //spooler.attach(10); //attaches spooler motor controller to pin 11
   auger.attach(9);  //attaches auger motor controller to pin 6
-  heater.attach(11);  //attaches heater motor controller to pin 5
+  //heater.attach(11);  //attaches heater motor controller to pin 5
  
   Serial.begin(9600);
 }
 
 void loop() 
 {  
-  //Serial.println(analogRead(1));
-  incomingbyte =Serial.read();
-  heat_level = incomingbyte;
-  //auger_speed = analogRead(2);
-  //spooler_speed = analogRead(1);
-  
   spooler_offset = (spooler_speed+90); //Conversion from servo to motor controller
   auger_offset = (auger_speed+90);  //Conversion for servo to motor controller
   heater_offset = (heat_level+90);  //Conversion for servo to motor controller
   
-  spooler.write(spooler_offset);  //Writes the spooler speed
+  //spooler.write(spooler_offset);  //Writes the spooler speed
   auger.write(auger_offset);  //Writes the auger speed
-  heater.write(heater_offset);  //Writes the heater speed
+  //heater.write(heater_offset);  //Writes the heater speed
 }
--- reprint/reprint.ino
+++ reprint/reprint.ino
@@ -18,8 +18,6 @@ int spooler_offset(0);
 int auger_offset(0);
 int heater_offset(0);
 
-int heatPin = 9;
-
 int tempSensor = 0;
 
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
@@ -73,7 +71,7 @@ void setup()
   
   //spooler.attach(9); //attaches spooler motor controller to pin 11
   //auger.attach(21);  //attaches auger motor controller to pin 6
-  //heater.attach(9);  //attaches heater motor controller to pin 5
+  heater.attach(9);  //attaches heater motor controller to pin 5
   
   //pinMode(heatPin, OUTPUT);
    
@@ -87,13 +85,9 @@ void setup()
 void loop()
 {    
   currTemp = temp.read_temp();
-  //Serial.println(currTemp);
+
   myPID.Compute(); //pid compute
-  
   heat_level = (Output/2.834); //conversion from 255 to 90 from PID
-  //Serial.println(heat_level);
-  Serial.write(heat_level);
-  //analogWrite(heatPin, heat_level);
   
   spooler_offset = (spooler_speed+90); //Conversion from servo to motor controller
   auger_offset = (auger_speed+90);  //Conversion for servo to motor controller
@@ -102,7 +96,7 @@ void loop()
   //spooler.write(spooler_offset);  //Writes the spooler speed
   //auger.write(auger_offset);      //Writes the auger speed
   //heater.write(heater_offset);    //Writes the heater speed
-  //heater.writeMicroseconds(1700);
+  
   switch (screen){
     case 0:
       lcd.clear();
